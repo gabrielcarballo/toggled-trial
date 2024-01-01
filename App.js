63f1f44e030b2907b709274a333e83f42e01c851
gabrielcarballo <gabrielfpcarballo@gmail.com>
2024-01-01 15:30:13 +0100

    Remove unused styles in App.js

--- App.js
+++ App.js
@@ -1,21 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View } from 'react-native';
 import HomeScreen  from './screens/home';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <View>
+      <StatusBar style="light" />
       <HomeScreen />
-      <StatusBar style="auto" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
